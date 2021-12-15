3e57d2572dcab9758630e42f8f01b32aa274a10d
EnricoDeg <degregori@dkrz.de>
2021-12-15 16:15:15 +0100

    exec updated

--- src/test.cpp
+++ src/test.cpp
@@ -7,8 +7,9 @@ int main(int argc, char **argv)
 {
   MPI_Init(NULL, NULL);
 
+  std::string s = "structured";
   std::shared_ptr<CCCC> ccm;
-  ccm.reset(new CCCC(MPI_COMM_WORLD, 2));
+  ccm.reset(new CCCC(MPI_COMM_WORLD, 2, s));
 
   int i = ccm->intercomm_create(1, 1);
   i = ccm->intercomm_create(2, 1);
--- src/test.f90
+++ src/test.f90
@@ -4,13 +4,15 @@ program test
     implicit none
     type(cccc) :: f
     integer i, ierr;
+    character(len=100) :: backend
 
     call MPI_Init(ierr)
 
+    backend = "structured"
     ! Create an object of type cccc
-    f = cccc(MPI_COMM_WORLD, 1)
+    f = cccc(MPI_COMM_WORLD, 1, trim(backend))
 
-    i = f%intercomm_create(1, 1)
+    !i = f%intercomm_create(1, 1)
 
     call f%finalize
 
