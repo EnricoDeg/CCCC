76688aac93fbc7a46eb19b0da7dcaf21451d51ac
EnricoDeg <degregori@dkrz.de>
2022-03-29 18:10:03 +0200

    full fortran test with fields, vars and cmds

--- src/test.f90
+++ src/test.f90
@@ -3,10 +3,12 @@ module func_pointer_mod
    use, intrinsic :: iso_c_binding
    use mpi
    implicit none
-
+   private
    double precision, allocatable, dimension(:,:) :: a
-   integer, parameter :: N = 1000
-
+   integer :: idx
+   integer, parameter :: N = 3
+   PUBLIC :: allocate_variable, initialize_variable, compute_variable, &
+             deallocate_variable
 contains
 
     subroutine hello_dummy() bind(C)
@@ -27,7 +29,7 @@ contains
         double precision :: ts, te
 
         write(*,*) "initialization"
-        
+        idx = 0
         ts = MPI_Wtime()
         do j = 1,N
             do i = 1,N
@@ -38,6 +40,15 @@ contains
         write(*,*) "timer = ", te - ts
     end subroutine initialize_variable
 
+    SUBROUTINE compute_variable() bind(C)
+        idx = idx + 1
+        a(:,:) = a(:,:) + idx
+    END SUBROUTINE compute_variable
+
+    SUBROUTINE deallocate_variable() bind(C)
+        deallocate(a)
+    END SUBROUTINE deallocate_variable
+
 end module func_pointer_mod
 
 program test
@@ -45,57 +56,87 @@ program test
     USE mpi
     use libcccc
     use, intrinsic :: iso_c_binding
-    !use func_pointer_mod
+    use func_pointer_mod
     implicit none
 
     integer, parameter :: dp = selected_real_kind(15)
     type(cccc) :: f
     integer i, ierr;
     character(len=100) :: backend
-    double precision, target, allocatable, dimension(:,:) :: a
-    integer myid;
-    integer, parameter :: N = 10
-!    real(dp), target, allocatable, dimension(:) :: z
+    double precision, target, allocatable, dimension(:,:) :: aaa
+    double precision, target, allocatable, dimension(:,:,:) :: bbb
+    real(dp), target, allocatable, dimension(:) :: z
+    integer :: myid, t
+    integer, parameter :: ND = 10
+    integer, parameter :: SL = 1 ! surface level
+    integer, parameter :: VL = 2 ! vertical levels
+    integer, parameter :: VAR_KERNEL = 1 ! VAR kernel ID
+    integer, parameter :: NPROCS_MAIN = 1 ! MPI tasks model
+    integer, parameter :: NPROCS_VAR_KERNEL = 1 ! MPI tasks VAR kernel
+    integer, parameter :: CMD_INIT_ID = 1
+    integer, parameter :: CMD_COMP_ID = 2
+    integer, parameter :: CMD_FIN_ID  = 3
+    integer, parameter :: EXG_ID = 1
+    integer, parameter :: NSTEPS = 10 ! Time loop steps
 
     call MPI_Init(ierr)
-
-    backend = "structured"
-    ! Create an object of type cccc
-    f = cccc(MPI_COMM_WORLD, 1, trim(backend))
-
-    call f%intercomm_create(1, 1)
-
-    allocate(a(N,N))
-    a(:,:) = 0.0e0
     call MPI_COMM_RANK(MPI_COMM_WORLD, myid, ierr)
-    CALL f%grid_subdomain_start(1, 1)
-    CALL f%grid_subdomain_end(N, N)
-    CALL f%grid_subdomain_ext(N, N)
-    CALL f%grid_subdomain_off(0, 0)
-    CALL f%grid_domain_ext(N, N)
-!    call f%set_redist(1, 1, MPI_DOUBLE_PRECISION)
 
-    if (myid == 0) a(:,:) = 1.0e0
+    backend = "structured" ! model grid type
+    ! Create an object of type cccc
+    f = cccc(MPI_COMM_WORLD, NPROCS_MAIN, trim(backend))
 
-    CALL f%add_field(C_LOC(a), 1, 1, 1, .true.)
+    call f%intercomm_create(VAR_KERNEL, NPROCS_VAR_KERNEL)
 
-!    allocate(z(1))
-!    call f%add_command(c_funloc(allocate_variable), 1, 1)
-!    call f%add_command(c_funloc(initialize_variable), 1, 1)
-!    z = 0.0e0
-!    call f%add_variable(z, 1, 1, 1, .true.)
+    ! field allocated and initialized
+    allocate(aaa(ND,ND), bbb(ND,ND,VL))
+    aaa(:,:) = 0.0e0
+    bbb(:,:,:) = 0.0e0
 
-    call f%start_concurrent(1)
-!    if (.not. f%has_kernel_role()) then
-!      z = 1.0e0
-!      write(*,*) "Model procs print this string"
-    call f%exchange_m2k(1, 1)
-!      call f%execute(1,1)
-!    end if
-    call f%stop_concurrent(1)
+    ! initialize grid info
+    CALL f%grid_subdomain_ext(ND, ND)
 
-!    write(*,*) "z = ", z
+    ! add fields 
+    CALL f%add_field(aaa, SL, VAR_KERNEL, EXG_ID, .true.) ! .true. means m2k
+    CALL f%add_field(bbb, VL, VAR_KERNEL, EXG_ID, .true.) ! .true. means m2k
 
+    ! variable allocated and initialized
+    allocate(z(1))
+    z = 0.0e0
+
+    ! add commands
+    call f%add_command(c_funloc(allocate_variable)  , VAR_KERNEL, CMD_INIT_ID)
+    call f%add_command(c_funloc(initialize_variable), VAR_KERNEL, CMD_INIT_ID)
+    call f%add_command(c_funloc(compute_variable)  , VAR_KERNEL, CMD_COMP_ID)
+    call f%add_command(c_funloc(deallocate_variable), VAR_KERNEL, CMD_FIN_ID )
+
+    ! add variables
+    call f%add_variable(z, SIZE(z), VAR_KERNEL, 1, .true.) ! .true. means m2k
+
+    ! CONCURRENT EXECUTION BEGIN
+    call f%start_concurrent(VAR_KERNEL)
+    if (.not. f%has_kernel_role()) then
+      ! variables and fields modified during main model time loop and used 
+      ! as input for the kernel duyring each time step
+      z = 1.0e0
+      aaa(:,:) = 1.0e0
+      bbb(:,:,:) = 2.0e0
+      ! initialize the kernel (fields only allocated on kernel procs)
+      call f%execute(VAR_KERNEL, CMD_INIT_ID)
+      ! Time loop
+      do t = 1,NSTEPS
+        call f%exchange_m2k(VAR_KERNEL, EXG_ID)
+        call f%execute(VAR_KERNEL, CMD_COMP_ID)
+        aaa = aaa + t
+        bbb = bbb + 2 * t
+        z   = z   + t / 2.0e0
+      end do
+      call f%execute(VAR_KERNEL, CMD_FIN_ID)
+    end if
+    call f%stop_concurrent(VAR_KERNEL)
+    ! CONCURRENT EXECUTION END
+
+    ! destroy cccc type object
     call f%finalize
 
     call MPI_Finalize(ierr)
